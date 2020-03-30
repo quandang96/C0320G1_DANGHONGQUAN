b729eecfb6ecefbe8cabe4c04c39146ff14dffe2
quandang1996 <quandanght@gmail.com>
2020-03-30 22:17:42 +0700

    code

---
+++ module1/2-BienKieuDuLieuVaToanTu/ThucHanh/2-SuDungToanTu/sudungtoantu.js
@@ -0,0 +1,13 @@
+let inputWitdth;
+let inputHeight;
+
+inputWitdth = prompt("Enter the width");
+inputHeight = prompt("Enter thr height");
+let width = parseInt(inputWitdth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
+
+
+
+
