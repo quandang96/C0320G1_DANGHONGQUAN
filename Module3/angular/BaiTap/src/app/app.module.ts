4bacbe26f906fb06e2b4e89478c8694b530c819f
quandang1996 <quandanght@gmail.com>
2020-07-12 16:06:21 +0700

    code

--- Module3/angular/BaiTap/src/app/app.module.ts
+++ Module3/angular/BaiTap/src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CalculatorComponent } from './calculator/calculator.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CalculatorComponent } from "./calculator/calculator.component";
+import { ColorPickerComponent } from "./color-picker/color-picker.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CalculatorComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CalculatorComponent, ColorPickerComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ Module3/angular/BaiTap/src/app/color-picker/color-picker.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-color-picker",
+  templateUrl: "./color-picker.component.html",
+  styleUrls: ["./color-picker.component.scss"],
+})
+export class ColorPickerComponent implements OnInit {
+  background = "#00e067";
+
+  onChange(value) {
+    this.background = value;
+  }
+  constructor() {}
+
+  ngOnInit() {}
+}
